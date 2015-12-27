388a54e904474f5941bfe6cd6f12e39787a051cc
Terry Cai <gidcai@gmail.com>
2015-12-27 20:42:36 +0800

    update gulp task

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # tech-logo
 some framework and development language logos
+
+##Install
+
+Ubuntu:
+
+```shell
+apt-get install imagemagick
+apt-get install graphicsmagick
+```
+
+Mac OS X (using [Homebrew](http://brew.sh/)):
+
+```shell
+brew install imagemagick
+brew install graphicsmagick
+```
+
+Windows & others:
+- GraphicsMagick: [http://www.graphicsmagick.org/download.html](http://www.graphicsmagick.org/download.html)
+- ImageMagick: [http://www.imagemagick.org/script/binary-releases.php](http://www.imagemagick.org/script/binary-releases.php)
+
+Confirm that GraphicsMagick or ImageMagick is properly set up by executing `gm -version` or `convert -version` in a terminal.
+
+##Task
+
+```shell
+npm install
+gulp --tasks
+
+```
+
+Task detail see the `gulpfile.js` file.
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+'use strict';
+
+var gulp = require('gulp');
+var gm = require('gulp-gm');
+var fs = require('fs');
+var path = require("path");
+var mkdirp = require('mkdirp');
+var async = require('async');
+
+var processlog = function(msg) {
+    var util = require('util');
+    var obj = {};
+    obj.inspect = function(depth) {
+        return "\u001b[33m " + msg + "\u001b[39m";
+    };
+    if (msg == "end") {
+        process.stdout.write("\n");
+    } else {
+        process.stdout.write(util.inspect(obj));
+
+    }
+};
+
+var orginalSize = "160x160";
+
+function getSize(size) {
+    size || (size = 72);
+    return size + "x" + size;
+}
+
+function getArgSize() {
+    var argv = process.argv.slice(3);
+    if (argv[0] == "-s" && argv[1]) {
+        return argv[1];
+    }
+}
+
+function handleImage(item, targetpath, distpath, size, mask, callback) {
+    var spawn = require('child_process').spawn;
+    mask = mask || "./dist/mask/" + orginalSize + "/mask-1.png";
+    var ps = spawn("convert", [
+        "-alpha", "set",
+        targetpath + item,
+        "-resize", "80%",
+        "-gravity", "center",
+        "-extent", orginalSize,
+        "background/1.png", "-compose", "DstOver", "-composite",
+        mask, "-compose", "DstIn", "-composite",
+        "-resize", getSize(size),
+        distpath + path.basename(item)
+    ]);
+    // ps.stdin.end();
+    ps.on('error', function() {
+        console.info('error');
+    })
+    ps.on('exit', function() {
+        callback();
+    })
+}
+
+
+gulp.task('default', function() {
+    return gulp.src('logo/*.png')
+        .pipe(gm(function(gmfile) {
+            return gmfile
+                .alpha('remove')
+                .resize(getSize())
+        }, {
+            imageMagick: true
+        }))
+        .pipe(gulp.dest('./dist/default/' + getSize()));
+});
+
+gulp.task('mask', function() {
+    return gulp.src('mask/*.png')
+        .pipe(gm(function(gmfile) {
+            return gmfile
+                .resize(orginalSize);
+        }, {
+            imageMagick: true
+        }))
+        .pipe(gulp.dest('./dist/mask/' + orginalSize));
+});
+
+
+gulp.task('beauty', ["mask"], function(done) {
+    var targetpath = "logo/";
+    var size = getArgSize() || 72;
+    var distpath = "./dist/beauty/" + getSize(size) + "/";
+    var mask = "./dist/mask/" + orginalSize + "/mask-1.png";
+    mkdirp.sync(distpath);
+    fs.readdir(targetpath, function(err, items) {
+        async.eachSeries(items, function iterator(item, callback) {
+            handleImage(item, targetpath, distpath, size, mask, callback);
+            processlog("...");
+        }, function asyncdone() {
+            processlog("end");
+            done();
+        });
+    });
+});
