4f380630eb4d09f0e47156970a9160bf2c9e0bc5
Terry Cai <gidcai@gmail.com>
2016-12-31 16:09:03 +0800

    add new logos

--- gulpfile.js
+++ gulpfile.js
@@ -24,7 +24,7 @@ var processlog = function(msg) {
 var orginalSize = "72x72";
 
 var fullBackground = ["tcl_tk", "react", "react_native", 
-    "typescript", "javascript","gcc", "dojo","rethinkdb","gnu_fortran", "fish"
+    "typescript", "javascript","gcc", "dojo","rethinkdb","gnu_fortran", "fish", "statsmodels"
 ];
 
 function getSize(size) {
