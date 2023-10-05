3c51dd38e6c9aef618417e2bfd3e9ce5b7f2a988
Terry Cai <gidcai@gmail.com>
2023-10-05 17:24:25 +0800

    feat: new logos

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 module.exports = {
   orginalSize: "72x72",
   fullBackground: ["prettier", "i3", "tcl_tk", "react", "react_native", 'd',
-    "typescript", "javascript", "gcc", "dojo", "rethinkdb", "gnu_fortran", "fish", "statsmodels"
+    "typescript", "javascript", "gcc", "dojo", "rethinkdb", "gnu_fortran", "fish", "statsmodels",
+    "wagtail", "qunit"
   ],
   normalDist: "./dist/default/",
   beautyTarget: "dist/default/72x72/",
